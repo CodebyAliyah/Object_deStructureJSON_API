041224754e699ea78ac9de32ec08b7eded9679f1
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-16 03:57:22 -0700

    Code Successfully

---
+++ object_deStructure_JSON_API.js
@@ -0,0 +1,27 @@
+
+
+const myObject = {
+  courseName : "JavaScript" ,
+  coursePrice : 999,
+  courseInstructer : "Hitesh" , 
+}
+
+// console.log(myObject.coursePrice)
+
+const {courseInstructer : instructer} = myObject
+console.log(instructer) ;
+
+
+/* *************  JSON ************ */
+
+// {
+//   "name" : "Ali Yahya" ,
+//    "age" : 21,
+//    "qualification" : "Graduate" ,
+// }
+
+// [
+//   {}
+
+//   {}
+// ]
